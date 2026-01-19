c74b111e86b5ab915790bcf613d064f5a7be46eb
Do Hoai Anh <anhd78428@gmail.com>
2026-01-19 12:45:41 +0700

    update index.ejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const userService = require('../services/userService');
 
 exports.index = (req, res) => {
-    res.render('index');
+    res.render('index', { user: req.session && req.session.user ? req.session.user : null });
 };
 
 exports.chat = async (req, res) => {
